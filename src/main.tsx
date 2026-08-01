7c0eadcda404f871df36c96d8a9d3d73704a16f1
GDowon <9363as@gmail.com>
2026-08-01 10:31:43 +0900

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 
   import { createRoot } from "react-dom/client";
   import App from "./app/App";
-  import "./styles/index.css";
+  import "./styles/index.css"; 
 
   const container = document.getElementById("root");
 
